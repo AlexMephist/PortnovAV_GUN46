7a95794213457d3898d9cbba55cc24e8c47d406f
Portnov AV Gun-46 <portnovalex0373@gmail.com>
2026-09-09 21:46:58 +0400

    add MyApp1_Classes

---
+++ MyApp1_Classes/MyApp1_Classes/Unit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp1_Classes
+{
+    public class Unit
+    {
+        private float _health;
+        private float _armor;
+
+        private int _damage;
+
+
+        // свойства
+
+        public string Name { get; }
+
+        public float Health => _health;
+
+        public int Damage => _damage;
+
+        public float Armor 
+        { 
+            get => (_armor); set => _armor = value;  
+        }
+        
+          
+        
+        // конструкторы
+
+        public Unit(): this(name: "Unknown Unit") 
+        { 
+        }
+
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public float GetRealHealth()
+        { 
+            Armor = 0.6f;
+            return Health * (1f + Armor);
+        }
+
+        public bool SetDamage(float Health, float Armor, float value)
+        {
+            _damage = 5;
+            Health = Health - value * Armor;
+            _health = Health;
+
+            if (Health <= 0f)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+           
+        }
+    }
+}
